81d7eb000b46c40a090000bfc19dc58dcd49c507
riyadahmed2809@gmail.com <92542706+riyad-ahmed@users.noreply.github.com>
2023-02-15 17:08:02 +0600

    clock new file added

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// dram javascript
+
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
@@ -19,3 +21,27 @@ keys.forEach(key => {
 });
 
 window.addEventListener('keydown',playSound);
+
+
+// clock javascript
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate (){
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const mins = now.getMinutes();
+  const minsDegrees = ((mins / 60) * 360) + 90;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  const hour = now.getHours();
+  const hourDegrees = ((hour / 12) * 360) + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
