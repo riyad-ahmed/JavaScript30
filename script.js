0c59b6d7e3141c3710412a636781adfcd7603c44
riyadahmed2809@gmail.com <92542706+riyad-ahmed@users.noreply.github.com>
2023-02-16 22:20:53 +0600

    up Variables added

--- script.js
+++ script.js
@@ -45,3 +45,13 @@ function setDate (){
 }
 
 setInterval(setDate, 1000);
+
+// upVariables JS
+
+const inputs=document.querySelectorAll('.controls input');
+function handleUpdate(){
+  const suffix=this.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+inputs.forEach(input=>input.addEventListener('change',handleUpdate));
+inputs.forEach(input=>input.addEventListener('mousemove',handleUpdate));
