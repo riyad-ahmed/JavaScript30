626fac62391c2387b9a8340b1649e982f4a34061
riyadahmed2809@gmail.com <92542706+riyad-ahmed@users.noreply.github.com>
2023-06-20 02:44:18 +0600

    data properly fetched

--- promise.js
+++ promise.js
@@ -1,9 +1,32 @@
+const accesskey = "ufBwR6u6nA9ZJBff10VoFTTuSL9uoBRFmgp_QAtqpt0";
+
+const secretkey = "j7XRD7IldLtrZvuJELyy3DuqaO5pxcMG7ubCcPNmk3I";
+
+
 const display = document.getElementById('display');
 
-function getData() {
-  fetch("https://unsplash.com/photos/AdkJ-LgpTrE")
-    .then(res => res.blod())
+async function getData() {
+  fetch(`https://api.unsplash.com/photos/?client_id=${accesskey}`)
+    .then(res => res.json())
     .then(data => {
-      display.innerHTML = data;
+      console.log(data);
+      display.src = data[5].urls.regular;
+      // display.innerHTML = data[0].urls.regular;
     })
 }
+
+
+// let inputData = "";
+
+// async function getData() {
+//   inputData = display.value;
+//   // const url = `https://api.unsplash.com/search/photos/?$query=${inputData}$client_id=${accesskey}`;
+//   const url = `https://api.unsplash.com/photos/?client_id=${accesskey}`
+//   const res = await fetch(url);
+//   const data = await response.json();
+
+//   const results = data.results;
+
+//   console.log(data);
+
+// }
