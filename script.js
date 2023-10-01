b1acda3c512457870e6c59816682e2080388d246
riyadahmed2809@gmail.com <92542706+riyad-ahmed@users.noreply.github.com>
2023-10-02 03:14:16 +0600

    new

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function playSound(e) {
 
 function removeTransition(e) {
   if (e.propertyName != 'transform') return;
-  this .classList.remove('playing');
+  this.classList.remove('playing');
 }
 
 
@@ -20,7 +20,7 @@ keys.forEach(key => {
   return key.addEventListener('transitionend', removeTransition)
 });
 
-window.addEventListener('keydown',playSound);
+window.addEventListener('keydown', playSound);
 
 
 // clock javascript
@@ -28,7 +28,7 @@ const secondHand = document.querySelector('.second-hand');
 const minsHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 
-function setDate (){
+function setDate() {
   const now = new Date();
 
   const seconds = now.getSeconds();
@@ -48,50 +48,12 @@ setInterval(setDate, 1000);
 
 // upVariables JS
 
-const inputs=document.querySelectorAll('.controls input');
-function handleUpdate(){
-  const suffix=this.dataset.sizing || '';
+const inputs = document.querySelectorAll('.controls input');
+function handleUpdate() {
+  const suffix = this.dataset.sizing || '';
   document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
-inputs.forEach(input=>input.addEventListener('change',handleUpdate));
-inputs.forEach(input=>input.addEventListener('mousemove',handleUpdate));
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
 
-// Array Cardio Practice
 
-const inventors = [
-  { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
-  { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
-  { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
-  { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
-  { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
-  { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
-  { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
-  { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
-  { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
-  { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
-  { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
-  { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
-  ];
-
-const people = [
-  'Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick',
-  'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire',
-  'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David',
-  'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana',
-  'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar',
-  'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric',
-  'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell',
-  'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph',
-  'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank',
-  'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony',
-  'Blake, William'
-];
-
-const fifteen = inventors.filter(function(inventor) {
-  if(inventor.year >= 1500 && inventor.year < 1600){
-    return true;
-  }
-});
-
-const ordered = inventors.sort((a,b) => a.year > b.year ? 1 : -1);
-console.table(ordered);
